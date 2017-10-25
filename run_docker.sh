b4522c4648c1138bd459cf57051bbabebb9aa705
Boncheol Gu <boncheol.gu@gmail.com>
2017-10-25 10:32:47 +0900

    Add script for running GUI apps in the container.

---
+++ run_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+xhost +local:docker
+
+nvidia-docker run \
+              -v /tmp/.X11-unix:/tmp/.X11-unix \
+              -v $HOME/.ssh:/home/user/.ssh \
+              -e DISPLAY=unix$DISPLAY \
+              --device /dev/dri \
+              $@
